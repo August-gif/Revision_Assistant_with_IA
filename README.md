fc3b06f1ea8670fe7b069c0704199d3823325786
auguste_watibi <augustinwatibi@gmail.com>
2025-04-10 23:57:54 +0200

    Mon premier commit du projet REV ASSISTANCE version REACT JS

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+#voici un ebauche du projet rev assistance réaliser avec reaact js+vite , il existe une autre version encours de developpement en version angular Typescript qui est presque dans sa phase finale que je ne manquerai pas de le push.
+
+#cette version est expose la page principale qque je vous invite à apporter les modifications ou avis pour parfaire cette version de REV ASSISTANCE avec React qui est si vite.
+#pour lancer le projet il faut exécuter la commande :
+    - npm install : pour installer les dependances du projets
+    - npm run dev: pour lancer le projet 
+#cette version utilise provisiorement l'API de gemini, qui permet de simuler le serveur IA de REV ASSISTANCE
+    
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,147 @@
+import React, {  useContext, useState, useEffect ,useRef} from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+
+const Main = () => {
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context);
+  const [contextMenuVisible, setContextMenuVisible] = useState(false);
+  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
+  const userIconRef = useRef(null);
+
+  // Fermer le menu si on clique ailleurs
+  useEffect(() => {
+    const handleClick = () => setContextMenuVisible(false);
+    window.addEventListener('click', handleClick);
+    return () => window.removeEventListener('click', handleClick);
+  }, []);
+ 
+  // Ouvrir le menu au double-clic
+  const handleDoubleClick = (e) => {
+    e.preventDefault(); // Empêche le menu contextuel par défaut
+    e.stopPropagation(); //  ça bloque le useEffect de tout fermer
+    if (userIconRef.current) {
+      const rect = userIconRef.current.getBoundingClientRect();
+      const top = rect.bottom + window.scrollY + 8; // 8px d'espacement sous l'image
+      const left = rect.right - 300; // on décale vers la gauche pour que le menu colle au bord droit de l'icône
+  
+      setContextMenuPosition({ x: left, y: top });
+      setContextMenuVisible(true);
+    }
+  };
+
+  // Actions des boutons
+  const exploreGpts = () => alert('Explore GPTs');
+  const customize = () => alert('Customize ChatGPT');
+  const settings = () => alert('Settings');
+  const upgrade = () => alert('Upgrade Plan');
+  const extension = () => alert('Install Extension');
+  const logout = () => alert('Logging out');
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Rev Assistance</p>
+        <img ref={userIconRef} src={assets.user_icon} alt="" onClick={handleDoubleClick} />
+        {contextMenuVisible && (
+         <div
+          className={`context-menu ${contextMenuVisible ? 'visible' : ''}`}
+          style={{ top: `${contextMenuPosition.y}px`, left: `${contextMenuPosition.x}px` }}
+         
+         >
+           <button onClick={exploreGpts}>
+             <i className="bx bx-planet"></i> Explore REV
+           </button>
+           <button onClick={customize}>
+             <i className="bx bx-slider-alt"></i> Customize REV
+           </button>
+           <button onClick={settings}>
+             <i className="bx bx-cog"></i> Settings
+           </button>
+           <hr />
+           <button onClick={upgrade}>
+             <i className="bx bx-up-arrow-alt"></i> Upgrade Plan
+           </button>
+           {/* <button onClick={extension}>
+             <i className="bx bx-extension"></i> Get Extension
+           </button> */}
+           <hr />
+           <button className="logout" onClick={logout}>
+             <i className="bx bx-log-out"></i> Log out
+           </button>
+         </div> 
+       )} 
+        
+      </div>
+      <div className="main-container">
+
+        {!showResult
+        ?<>
+        <div className="greet">
+          <p>
+            <span>Hello, Dev.</span>
+          </p>
+          <p>How can I help you today?</p>
+         </div>
+       {/*  <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div> */}
+        </>
+        :
+        <div className="result">
+          <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading
+            ?<div className="loader">
+              <hr />
+              <hr />
+              <hr />
+            </div>
+            :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+            }
+           
+          </div>
+
+
+        </div>
+        }
+       
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              {input?<img  onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}
+              
+            </div>
+          </div>
+          <p className="bottom-info">
+             Rev Assistance @copyright
+             Dev BrainTech Watibi
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
